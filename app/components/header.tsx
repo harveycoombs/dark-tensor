1b73e3c0fc26c48e69c57d397374f696832d1b08
harveycoombs <harveycoombs23@outlook.com>
2024-11-28 20:57:44 +0000

    header

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,9 +1,16 @@
 import Link from "next/link";
 
+import Button from "@/app/components/ui/button";
+
 export default function Header() {
     return (
-        <header className="p-4 select-none">
-            <Link href="/" className="font-bold">Congruence <span className="text-slate-400/60">{process.env.APP_VERSION}</span></Link>
+        <header className="p-3 select-none bg-white sticky top-0 border-b border-b-slate-300 flex justify-between items-center z-30">
+            <Link href="/" className="font-bold duration-100 hover:opacity-70 active:opacity-90">Congruence <span className="text-slate-400/60">{process.env.APP_VERSION}</span></Link>
+            <nav></nav>
+            <div>
+                <Button>Sign In</Button>
+                <Button transparent={true} classes="ml-1.5">Register</Button>
+            </div>
         </header>
     );
 }
--- app/components/ui/button.tsx
+++ app/components/ui/button.tsx
@@ -4,12 +4,12 @@ interface Properties {
     children: React.ReactNode;
     classes?: string;
     url?: string;
-    alt?: boolean;
+    transparent?: boolean;
     [key: string]: any;
 }
 
-export default function Button({ children, classes, url, alt, ...rest }: Properties) {
-    let appearance = alt ? "" : "bg-blue-600 text-white hover:bg-blue-700 active:bg-blue-800";
+export default function Button({ children, classes, url, transparent, ...rest }: Properties) {
+    let appearance = transparent ? "bg-transparent text-slate-600 font-semibold hover:bg-slate-100 hover:text-slate-500" : "bg-blue-600 text-white hover:bg-blue-700 active:bg-blue-800";
     let classList = `px-3.5 py-2.5 rounded-md text-[0.8rem] text-center font-medium leading-none duration-100 select-none ${appearance} ${classes?.length ? " " + classes : ""}`;
 
     return (
