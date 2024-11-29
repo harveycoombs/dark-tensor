789dd56220dac20d9147716ca9d1f9db816df849
harveycoombs <harveycoombs23@outlook.com>
2024-11-29 20:32:55 +0000

    Added shadow to fields when focused

--- app/components/ui/field.tsx
+++ app/components/ui/field.tsx
@@ -7,6 +7,6 @@ interface Properties {
 }
 
 export default function Field({ type, classes, ...rest }: Properties) {
-    let classList = `px-3.5 py-2 box-border rounded-md text-[0.8rem] border border-slate-300 bg-transparent font-medium leading-none duration-100 select-none focus:outline-none focus:border-blue-600 ${classes?.length ? " " + classes : ""}`;
+    let classList = `px-3.5 py-2 box-border rounded-md text-[0.8rem] border border-slate-300 bg-transparent font-medium leading-none duration-100 select-none focus:outline-none focus:border-blue-600 focus:shadow-md ${classes?.length ? " " + classes : ""}`;
     return <input type={type ?? "text"} className={classList} {...rest} />;
 }
