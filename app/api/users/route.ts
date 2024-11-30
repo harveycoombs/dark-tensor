e083627bfe082f28565ffe2577f40277f5c02430
Harvey Coombs <70911622+harveycoombs@users.noreply.github.com>
2024-11-30 14:50:28 +0000

    user creation & deletion functions/route

---
+++ app/api/users/route.ts
@@ -0,0 +1,50 @@
+import { NextResponse } from "next/server";
+
+import { emailExists, createUser, getUserByID, deleteUser } from "@/data/users";
+import { createJWT } from "@/data/jwt";
+
+export async function POST(request: Request): Promise<NextResponse> {
+    let data = await request.formData();
+
+    let email = data.get("email")?.toString();
+    let password = data.get("password")?.toString();
+    let firstName = data.get("firstname")?.toString();
+    let lastName = data.get("lastname")?.toString();
+    let location = data.get("location")?.toString();
+
+    if (!email?.length || !password?.length || !firstName?.length || !lastName?.length || !location?.length) {
+        return NextResponse.json({ error: "One or more fields were not provided." }, { status: 400 });
+    }
+
+    let emailAlreadyExists = await emailExists(email);
+    if (emailAlreadyExists) return NextResponse.json({ error: "Provided email address already exists." }, { status: 409 });
+
+    let userid = await createUser(email, password, firstName, lastName, location);
+
+    if (!userid) return NextResponse.json({ error: "Unable to create user." }, { status: 500 });
+
+    let user = await getUserByID(userid);
+    let credentials = createJWT(user);
+
+    let response = NextResponse.json({ success: true }, { status: 200 });
+
+    response.cookies.set("token", credentials.token, {
+        httpOnly: true,
+        secure: true,
+        maxAge: 3155760000
+    });
+
+    return response;
+}
+
+export async function DELETE(request: Request): Promise<NextResponse> {
+    let data = await request.formData();
+    let userid = parseInt(data.get("userid")?.toString() ?? "0");
+
+    let deleted = await deleteUser(userid);
+
+    let response = NextResponse.json({ success: deleted }, { status: 200 });
+    response.cookies.delete("token");
+
+    return response;
+}
--- data/users.ts
+++ data/users.ts
@@ -1,6 +1,18 @@
 "use server";
 import pool from "@/data/database";
-import { verify } from "@/data/passwords";
+import { generateHash, verify } from "@/data/passwords";
+
+export async function createUser(email: string, password: string, firstName: string, lastName: string, location: string): Promise<number> {
+    let passwordHash = await generateHash(password);
+    let [result]: any = await pool.query("INSERT INTO users (email_address, password_hash, first_name, last_name, location) VALUES (?, ?, ?, ?, ?)", [email, passwordHash, firstName, lastName, location]);
+    
+    return result.insertId ?? 0;
+}
+
+export async function deleteUser(userid: number): Promise<boolean> {
+    let [result]: any = await pool.query("UPDATE users SET deleted = 1 WHERE user_id = ?", [userid]);
+    return result.affectedRows > 0;
+}
 
 export async function getUserByID(userid: number): Promise<any> {
 	let [result]: any = await pool.query("SELECT user_id, creation_date, first_name, last_name, location FROM users WHERE user_id= ? AND deleted = 0", [userid]);
