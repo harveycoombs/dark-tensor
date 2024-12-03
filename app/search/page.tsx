7366f07072364e643910ed03aaa9baa6dbde7999
harveycoombs <harveycoombs23@outlook.com>
2024-12-03 18:23:17 +0000

    Removed unused import

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useRef, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import Link from "next/link";
 
