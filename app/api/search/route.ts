2569840c24125d7811c4a626db2372014c2f9c67
Harvey Coombs <70911622+harveycoombs@users.noreply.github.com>
2024-12-04 12:38:43 +0000

    Switched from using huggingface to ollama for model inference (2)

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -11,15 +11,12 @@ export async function GET(request: Request): Promise<NextResponse> {
     }
 
     try {
-        let model = await generateText({
-            model: "deepseek-ai/DeepSeek-V2-Lite",
-            prompt: query,
-            maxLength: 200
+        let response = await generateText({
+            model: "deepseek-v2:lite",
+            prompt: query
         });
 
-        console.log("model", model);
-
-        return NextResponse.json({ summary: "", results: [] }, { status: 200 });
+        return NextResponse.json({ summary: response ?? "", results: [] }, { status: 200 });
     } catch (ex: any) {
         return NextResponse.json({ error: `Unable to generate text: ${ex.message}.` }, { status: 500 });
     }
