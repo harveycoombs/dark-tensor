c6b9bd1dd195cf266e310a141a563340826e37f3
Harvey Coombs <70911622+harveycoombs@users.noreply.github.com>
2024-12-12 13:02:00 +0000

    Added about page

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+import Header from "@/app/components/header";
+
+export default function About() {
+    return (
+        <>
+            <Header />
+            <main></main>
+        </>
+    );
+}
