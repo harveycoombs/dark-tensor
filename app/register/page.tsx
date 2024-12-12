8632dc18a4c19d27866596419ec17ede7d37479b
Harvey Coombs <70911622+harveycoombs@users.noreply.github.com>
2024-12-12 13:24:40 +0000

    Padding adjustment for registration form

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -4,7 +4,7 @@ import RegistrationForm from "./form";
 export default function Register() {
     return (
         <main className="h-[calc(100vh-55px)] grid place-items-center">
-            <section className="text-center max-[530px]:w-full max-[530px]:px-4">
+            <section className="text-center max-[530px]:w-full max-[530px]:px-3.5">
                 <Link href="/" className="text-2xl font-bold select-none hover:opacity-85 active:opacity-70">Congruence <span className="text-blue-600">AI</span></Link>
                 <strong className="block text-sm font-medium select-none text-slate-400/60">Create an account</strong>
                 <RegistrationForm />
