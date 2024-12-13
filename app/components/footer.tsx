dd24703a6f45fcd30e1c2789fa2ccea2d75ecec4
harveycoombs <harveycoombs23@outlook.com>
2024-12-13 13:39:55 +0000

    Added former name to footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
 
 export default function Footer() {
-    return <footer className="p-4 text-sm text-slate-400/60 font-medium select-none text-center">&copy; {new Date().getFullYear()} Collate AI &middot; <Link href="https://harveycoombs.com/" target="_blank" className="hover:underline">Harvey Coombs</Link></footer>;
+    return <footer className="p-4 text-sm text-slate-400/60 font-medium select-none text-center">&copy; {new Date().getFullYear()} Collate &#40;Formerly Congruence&#41; &middot; <Link href="https://harveycoombs.com/" target="_blank" className="hover:underline">Harvey Coombs</Link></footer>;
 }
