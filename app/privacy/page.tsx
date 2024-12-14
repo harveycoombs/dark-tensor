5eb14e8b39ed4996eec57fa0f3e26807a9e9fe0a
harveycoombs <harveycoombs23@outlook.com>
2024-12-14 21:56:38 +0000

    Internal page placeholder content

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -4,7 +4,12 @@ export default function Privacy() {
     return (
         <>
             <Header />
-            <main></main>
+            <main className="h-[calc(100vh-110px)] py-4 grid">
+                <section className="w-650 mx-auto mt-24 max-[700px]:w-full max-[700px]:px-3">
+                    <h1 className="text-4xl text-center font-semibold mb-4 select-none">Privacy Policy</h1>
+                    <p className="text-sm text-slate-400/80 leading-relaxed">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni alias culpa excepturi labore blanditiis, ex esse ut. Doloribus, dolorum consequatur sint rem corporis expedita voluptatibus est at voluptatem numquam tempora ea hic aperiam quibusdam, doloremque suscipit sunt dicta fuga delectus! Beatae esse velit exercitationem recusandae sint ullam molestias, quidem quia nisi consequatur animi et odio rem delectus explicabo quo provident minus distinctio nesciunt expedita. Amet ad, modi sed doloremque earum, asperiores error deleniti, ipsa harum atque minima saepe necessitatibus. Velit molestiae aliquid minus error, at ratione, est reprehenderit totam rem optio dicta dolorem molestias, corrupti atque iste ab! Nulla molestias praesentium reiciendis accusantium expedita porro sint laborum tempora fuga similique et, repellendus alias consequuntur voluptas fugiat! Sapiente consequatur neque dignissimos! Officiis error necessitatibus reprehenderit voluptatum perferendis. Minus omnis quia, dignissimos necessitatibus reiciendis suscipit repellat voluptatibus accusantium nostrum earum similique iure quo eius. Cumque dolorum recusandae doloremque deserunt, iste provident quae doloribus corrupti quod. Natus temporibus repellendus excepturi nostrum minus officiis necessitatibus. Assumenda cupiditate, eos obcaecati qui officiis iure, a inventore sed dolore eaque quia alias. Nihil corporis laborum pariatur reiciendis dolor, ipsum tenetur illum magnam nam dignissimos consectetur ea temporibus, accusantium beatae veritatis et dolorem voluptatum ipsam laboriosam aliquam excepturi.</p>
+                </section>
+            </main>
         </>
     );
 }
