32def099418ec4d87c9e53b8f9db8b869704b1be
harveycoombs <harveycoombs23@outlook.com>
2024-12-14 22:15:16 +0000

    Created reusable internal page components

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,15 +1,11 @@
-import Header from "@/app/components/header";
+import InternalPage, { PageSection } from "@/app/components/common/internal";
 
 export default function About() {
     return (
-        <>
-            <Header />
-            <main className="h-[calc(100vh-110px)] py-4 grid">
-                <section className="w-650 mx-auto mt-24 max-[700px]:w-full max-[700px]:px-3">
-                    <h1 className="text-4xl text-center font-semibold mb-4 select-none">About Collate</h1>
-                    <p className="text-sm text-slate-400/80 leading-relaxed">Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni alias culpa excepturi labore blanditiis, ex esse ut. Doloribus, dolorum consequatur sint rem corporis expedita voluptatibus est at voluptatem numquam tempora ea hic aperiam quibusdam, doloremque suscipit sunt dicta fuga delectus! Beatae esse velit exercitationem recusandae sint ullam molestias, quidem quia nisi consequatur animi et odio rem delectus explicabo quo provident minus distinctio nesciunt expedita. Amet ad, modi sed doloremque earum, asperiores error deleniti, ipsa harum atque minima saepe necessitatibus. Velit molestiae aliquid minus error, at ratione, est reprehenderit totam rem optio dicta dolorem molestias, corrupti atque iste ab! Nulla molestias praesentium reiciendis accusantium expedita porro sint laborum tempora fuga similique et, repellendus alias consequuntur voluptas fugiat! Sapiente consequatur neque dignissimos! Officiis error necessitatibus reprehenderit voluptatum perferendis. Minus omnis quia, dignissimos necessitatibus reiciendis suscipit repellat voluptatibus accusantium nostrum earum similique iure quo eius. Cumque dolorum recusandae doloremque deserunt, iste provident quae doloribus corrupti quod. Natus temporibus repellendus excepturi nostrum minus officiis necessitatibus. Assumenda cupiditate, eos obcaecati qui officiis iure, a inventore sed dolore eaque quia alias. Nihil corporis laborum pariatur reiciendis dolor, ipsum tenetur illum magnam nam dignissimos consectetur ea temporibus, accusantium beatae veritatis et dolorem voluptatum ipsam laboriosam aliquam excepturi.</p>
-                </section>
-            </main>
-        </>
+        <InternalPage title="About Collate">
+            <PageSection title="What is Collate?">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nihil blanditiis cupiditate in dolorum obcaecati ex commodi architecto, voluptatum officia, velit minima, corrupti pariatur asperiores? Vero recusandae totam, error rem neque reprehenderit iusto numquam blanditiis mollitia quae? Consectetur dolor beatae non quia cum laudantium doloremque nulla maxime adipisci delectus est sequi numquam consequuntur debitis blanditiis voluptate suscipit officiis, ratione necessitatibus soluta, possimus esse et. Facilis dolor quia et qui corporis deserunt, accusantium reprehenderit nihil sapiente sint debitis veniam quidem dolorem architecto, distinctio earum ipsam? Quae sint eos est veniam omnis eum soluta culpa asperiores totam sunt, similique ad quas quia incidunt aliquid repudiandae autem dignissimos modi quaerat quisquam? Doloremque officia aspernatur quae molestiae fugit debitis eum neque architecto natus vero.
+            </PageSection>
+        </InternalPage>
     );
 }
---
+++ app/components/common/internal.tsx
@@ -0,0 +1,27 @@
+import Header from "@/app/components/header";
+
+interface Properties {
+    title: string;
+    children: React.ReactNode;
+}
+
+export default function InternalPage({ title, children }: Properties) {
+    return (
+        <>
+            <Header />
+            <main className="h-[calc(100vh-110px)] py-4">
+                <h1 className="block w-650 mx-auto mt-24 text-4xl text-center font-semibold select-none max-[700px]:w-full max-[700px]:px-3">{title}</h1>
+                {children}
+            </main>
+        </>
+    );
+}
+
+export function PageSection({ title, children }: Properties) {
+    return (
+        <section className="w-650 mx-auto mt-4 max-[700px]:w-full max-[700px]:px-3">
+            <h2>{title}</h2>
+            <div className="text-sm text-slate-400/80 leading-relaxed">{children}</div>
+        </section>
+    );
+}
