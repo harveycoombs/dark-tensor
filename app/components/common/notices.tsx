395644eaab33af3a592b2a17a4570d8cf15dbedf
harveycoombs <harveycoombs23@outlook.com>
2024-12-21 22:18:38 +0000

    Login form improvements

--- app/components/common/notices.tsx
+++ app/components/common/notices.tsx
@@ -4,13 +4,14 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 interface Properties {
     text?: string;
     icon?: any;
+    small?: boolean;
     classes?: string;
 }
 
-export function Error({ text, icon, classes }: Properties) {
-    return <div className={`block rounded-md px-4 py-3 bg-red-50 text-sm text-center leading-none text-red-500 font-semibold select-none${classes?.length ? " " + classes : ""}`}><FontAwesomeIcon icon={icon ?? faCircleExclamation} className="mr-0.5" /> {text ?? "Something went wrong"}</div>
+export function Error({ text, icon, small, classes }: Properties) {
+    return <div className={`block rounded-md bg-red-50 ${small ? "px-3 py-2 text-xs" : "px-4 py-3 text-sm"} text-center leading-none text-red-500 font-semibold select-none${classes?.length ? " " + classes : ""}`}><FontAwesomeIcon icon={icon ?? faCircleExclamation} className="mr-0.5" /> {text ?? "Something went wrong"}</div>
 }
 
-export function Warning({ text, icon, classes }: Properties) {
-    return <div className={`block rounded-md px-4 py-3 bg-amber-50 text-sm text-center leading-none text-amber-500 font-semibold select-none${classes?.length ? " " + classes : ""}`}><FontAwesomeIcon icon={icon ?? faWarning} className="mr-0.5" /> {text ?? "Something went wrong"}</div>
+export function Warning({ text, icon, small, classes }: Properties) {
+    return <div className={`block rounded-md bg-amber-50 ${small ? "px-3 py-2 text-xs" : "px-4 py-3 text-sm"} text-center leading-none text-amber-500 font-semibold select-none${classes?.length ? " " + classes : ""}`}><FontAwesomeIcon icon={icon ?? faWarning} className="mr-0.5" /> {text ?? "Something went wrong"}</div>
 }
--- app/login/form.tsx
+++ app/login/form.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import Field from "@/app/components/common/field";
 import Label from "@/app/components/common/label";
 import Button from "@/app/components/common/button";
+import { Error, Warning } from "@/app/components/common/notices";
 
 export default function LoginForm() {
     let [email, setEmail] = useState<string>("");
@@ -12,7 +13,6 @@ export default function LoginForm() {
     let [errorExists, setErrorExistence] = useState<boolean>(false);
     let [warningExists, setWarningExistence] = useState<boolean>(false);
     let [loading, setLoading] = useState<boolean>(false);
-    let [disabled, setDisability] = useState<boolean>(false);
 
     let [feedback, setFeedback] = useState<React.JSX.Element|null>(null);
 
@@ -20,34 +20,27 @@ export default function LoginForm() {
         e.preventDefault();
 
         setErrorExistence(false);
-        setDisability(true);
+        setWarningExistence(false);
+        setLoading(true);
 
-        let credentials = new URLSearchParams({ email, password });
+        let response = await fetch("/api/users/sessions", {
+            method: "POST",
+            body: new URLSearchParams({ email, password })
+        });
 
-        try {  
-            let response = await fetch("/api/users/sessions", {
-                method: "POST",
-                body: credentials
-            });
+        let json = await response.json();
 
-            let json = await response.json();
+        if (!response.ok || json.error) {
+            setErrorExistence(response.status != 400);
+            setWarningExistence(response.status == 400);
+            setLoading(false);
 
-            if (!json.success) {
-                setErrorExistence(false);
-                setWarningExistence(true);
-                setDisability(false);
+            setFeedback((response.status == 400) ? <Warning text="Invalid credentials" small={true} classes="mt-6" /> : <Error small={true} classes="mt-6" />);
 
-                setFeedback(<div className="text-sm font-medium text-center text-amber-400">Invalid credentials</div>);
-
-                return;
-            }
-
-            window.location.href = "/";
-        } catch {
-            setWarningExistence(false);
-            setErrorExistence(true);
-            setFeedback(<div className="text-sm font-medium text-center text-red-500">Something went wrong</div>);
+            return;
         }
+
+        window.location.href = "/";
     }
 
     function updateField(name: string, value: string) {
@@ -69,11 +62,11 @@ export default function LoginForm() {
         <form onSubmit={login} className="w-60">
             {feedback}
             <Label error={errorExists} warning={warningExists} classes="mt-6">Email Address</Label>
-            <Field classes={`block w-full${disabled ? " pointer-events-none" : ""}`} disabled={disabled} type="email" error={errorExists} warning={warningExists} onInput={(e: any) => updateField("email", e.target.value)} />
+            <Field classes="block w-full" disabled={loading} type="email" error={errorExists} warning={warningExists} onInput={(e: any) => updateField("email", e.target.value)} />
             <Label error={errorExists} warning={warningExists} classes="mt-2.5">Password</Label>
-            <Field classes={`block w-full${disabled ? " pointer-events-none" : ""}`} disabled={disabled} type="password" error={errorExists} warning={warningExists} onInput={(e: any) => updateField("password", e.target.value)} />
-            <Button classes="block w-full mt-2.5">Continue</Button>
-            <Button classes={`block w-full mt-2.5${disabled ? " pointer-events-none" : ""}`} disabled={disabled} transparent={true} url="/register">Register</Button>
+            <Field classes="block w-full" disabled={loading} type="password" error={errorExists} warning={warningExists} onInput={(e: any) => updateField("password", e.target.value)} />
+            <Button classes="block w-full mt-2.5" disabled={loading} loading={loading}>Continue</Button>
+            <Button classes="block w-full mt-2.5" disabled={loading} transparent={true} url="/register">Register</Button>
         </form>
     );
 }
