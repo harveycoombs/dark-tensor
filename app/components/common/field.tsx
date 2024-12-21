45ec9794c060ca7332c250713ee6a3368d9b52e6
harveycoombs <harveycoombs23@outlook.com>
2024-12-21 22:25:25 +0000

    Added error/warning border colors to field component

--- app/components/common/field.tsx
+++ app/components/common/field.tsx
@@ -7,6 +7,6 @@ interface Properties {
 }
 
 export default function Field({ type, classes, error, warning, ...rest }: Properties) {
-    let classList = `px-3.5 py-2 box-border rounded-md text-[0.8rem] border border-slate-300 bg-transparent font-medium leading-none duration-100 select-none focus:outline-none focus:border-sky-500 focus:shadow-md ${classes?.length ? " " + classes : ""}`;
-    return <input type={type ?? "text"} className={classList} {...rest} />;
+    let color = error ? "border-red-500" : warning ? "border-amber-500" : "border-slate-300";
+    return <input type={type ?? "text"} className={`px-3.5 py-2 box-border rounded-md text-[0.8rem] border ${color} bg-transparent font-medium leading-none duration-100 select-none focus:outline-none focus:border-sky-500 focus:shadow-md ${classes?.length ? " " + classes : ""}`} {...rest} />;
 }
