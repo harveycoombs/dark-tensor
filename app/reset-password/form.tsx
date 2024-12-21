ce3dff9384d49d456d9541f65779ee9a82ab8cf5
harveycoombs <harveycoombs23@outlook.com>
2024-12-21 21:45:50 +0000

    password reset form (1)

---
+++ app/reset-password/form.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { useState } from "react";
+
+import Field from "@/app/components/common/field";
+import Label from "@/app/components/common/label";
+import Button from "@/app/components/common/button";
+
+export default function PasswordResetForm() {
+    let [email, setEmail] = useState<string>("");
+
+    let [errorExists, setErrorExistence] = useState<boolean>(false);    
+    let [disabled, setDisability] = useState<boolean>(false);
+
+    let [feedback, setFeedback] = useState<React.JSX.Element|null>(null);
+    let [button, setButton] = useState<React.JSX.Element>(<Button classes="block w-full mt-2.5" disabled={disabled}>Continue</Button>);
+
+    async function send(e: any) {
+        e.preventDefault();
+    }
+
+    return (
+        <form onSubmit={send} className="w-60">
+            {feedback}
+            <Label error={errorExists} classes="mt-6">Email Address</Label>
+            <Field classes="block w-full" type="email" error={errorExists} onInput={(e: any) => setEmail(e.target.value)} />
+            {button}
+        </form>
+    );
+}
--- app/reset-password/page.tsx
+++ app/reset-password/page.tsx
@@ -1,5 +1,19 @@
+import Link from "next/link";
+import PasswordResetForm from "./form";
+
+import Logo from "@/app/components/common/logo";
+
 export default function ResetPassword() {
     return (
-        <main></main>
+        <main className="min-h-[calc(100vh-55px)] grid place-items-center">
+            <section className="text-center">
+                <Link href="/" className="block w-fit mx-auto mb-2 duration-100 hover:opacity-85 active:opacity-70" title="Collate AI"><Logo width={157} height={36} /></Link>
+                <strong className="block text-sm font-medium select-none text-slate-400/60">Reset Password</strong>
+                <PasswordResetForm />
+                <div className="text-[0.825rem] font-medium text-slate-400 mt-3 text-center select-none">
+                    <Link href="mailto:issues@collate.run" className="hover:underline">Report An Issue</Link>
+                </div>
+            </section>
+        </main>
     );
 }
