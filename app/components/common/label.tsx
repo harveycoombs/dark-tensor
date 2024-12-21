8a3120cf27eb8afdf4b8b1a2d846049ee78e6ad3
harveycoombs <harveycoombs23@outlook.com>
2024-12-21 22:47:52 +0000

    Added optional required asterisk to label component

--- app/components/common/label.tsx
+++ app/components/common/label.tsx
@@ -3,10 +3,11 @@ interface Properties {
     classes?: string;
     error?: boolean;
     warning?: boolean;
+    required?: boolean;
 }
 
-export default function Label({ children, classes, error, warning, ...rest }: Properties) {
+export default function Label({ children, classes, error, warning, required, ...rest }: Properties) {
     let color = error ? "text-red-500" : warning ? "text-amber-500" : "text-slate-400";
 
-    return <label className={`text-[0.78rem] font-medium select-none ${color} text-left block mb-0.5${classes?.length ? " " + classes : ""}`} {...rest}>{children}</label>;
+    return <label className={`text-[0.78rem] font-medium select-none ${color} text-left block mb-0.5${classes?.length ? " " + classes : ""}`} {...rest}>{children}{required ? <span>&#42;</span> : null}</label>;
 }
