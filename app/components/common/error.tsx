b7b41e267d9d33ec1bb9ac146d7f31943274407d
harveycoombs <harveycoombs23@outlook.com>
2024-12-21 20:53:05 +0000

    Updated padding of error messages

--- app/components/common/error.tsx
+++ app/components/common/error.tsx
@@ -8,5 +8,5 @@ interface Properties {
 }
 
 export default function Error({ text, icon, classes }: Properties) {
-    return <div className={`block rounded-md px-3 py-2 bg-red-50 text-sm text-center leading-none text-red-500 font-semibold select-none${classes?.length ? " " + classes : ""}`}><FontAwesomeIcon icon={icon ?? faCircleExclamation} className="mr-0.5" /> {text ?? "Something went wrong"}</div>
+    return <div className={`block rounded-md px-4 py-3 bg-red-50 text-sm text-center leading-none text-red-500 font-semibold select-none${classes?.length ? " " + classes : ""}`}><FontAwesomeIcon icon={icon ?? faCircleExclamation} className="mr-0.5" /> {text ?? "Something went wrong"}</div>
 }
