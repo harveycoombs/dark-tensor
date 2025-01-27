dc68373b3b2743cbeb64f5995b49e5a5f41e2115
harveycoombs <harveycoombs23@outlook.com>
2025-01-27 19:17:15 +0000

    toggle switch background color change

--- app/components/common/switch.tsx
+++ app/components/common/switch.tsx
@@ -8,7 +8,7 @@ export default function Switch({ checked, ...rest }: Properties) {
     let [isChecked, setIsChecked] = useState<boolean>(checked ?? false);
 
     return (
-        <div className={`w-12 h-7 p-1 rounded-md ${isChecked ? "bg-sky-500" : "bg-slate-300"} cursor-pointer`} onClick={() => setIsChecked(!isChecked)} {...rest}>
+        <div className={`w-12 h-7 p-1 rounded-md ${isChecked ? "bg-sky-500" : "bg-slate-300/70"} cursor-pointer`} onClick={() => setIsChecked(!isChecked)} {...rest}>
             <div className={`w-1/2 h-full rounded bg-white duration-100 ${isChecked ? "translate-x-full" : ""}`}></div>
         </div>
     );
