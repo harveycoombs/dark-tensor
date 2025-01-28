e42a7c62d4fade7f0a7bc0588fe26f6dda61c725
Harvey Coombs <70911622+harveycoombs@users.noreply.github.com>
2025-01-28 13:37:00 +0000

    About page content

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,10 +1,13 @@
+import Link from "next/link";
 import InternalPage, { PageSection } from "@/app/components/common/internal";
 
 export default function About() {
     return (
         <InternalPage title="About Collate">
             <PageSection title="What is Collate?">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro nihil blanditiis cupiditate in dolorum obcaecati ex commodi architecto, voluptatum officia, velit minima, corrupti pariatur asperiores? Vero recusandae totam, error rem neque reprehenderit iusto numquam blanditiis mollitia quae? Consectetur dolor beatae non quia cum laudantium doloremque nulla maxime adipisci delectus est sequi numquam consequuntur debitis blanditiis voluptate suscipit officiis, ratione necessitatibus soluta, possimus esse et. Facilis dolor quia et qui corporis deserunt, accusantium reprehenderit nihil sapiente sint debitis veniam quidem dolorem architecto, distinctio earum ipsam? Quae sint eos est veniam omnis eum soluta culpa asperiores totam sunt, similique ad quas quia incidunt aliquid repudiandae autem dignissimos modi quaerat quisquam? Doloremque officia aspernatur quae molestiae fugit debitis eum neque architecto natus vero.
+                <p>Collate is an AI search and summarisation engine designed to quickly deliver information, without clutter or inaccuracy. Leveraging the latest deep learning models, Collate provides a clean and efficient means of browsing the web.</p>
+                <br/>
+                <p>Collate was developed and is maintained by <Link href="https://harveycoombs.com" target="_blank" rel="noopener" className="font-medium hover:underline">Harvey Coombs</Link>.</p>
             </PageSection>
         </InternalPage>
     );
