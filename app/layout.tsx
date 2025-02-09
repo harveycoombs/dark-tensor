2d6db4fbd643ebaf86e17d0317676f13b962b6f7
harveycoombs <harveycoombs23@outlook.com>
2025-02-09 21:23:34 +0000

    removal of old twitter

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,8 +32,7 @@ export const metadata: Metadata = {
     twitter: {
         card: "summary_large_image",
         title: "Collate AI",
-        description: description,
-        creator: "@harveycoombs23"
+        description: description
     }
 };
 
