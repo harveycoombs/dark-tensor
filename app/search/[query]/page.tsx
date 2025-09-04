e247eb286a71df8511109c7ec60144712e0e9618
Harvey Coombs <harveycoombs23@outlook.com>
2025-09-04 22:23:30 +0100

    Improved handling of search summary

--- app/search/[query]/page.tsx
+++ app/search/[query]/page.tsx
@@ -12,7 +12,8 @@ import { Error } from "@/app/components/common/Notices";
 
 export default function Search(e: any) {
     const [query, setQuery] = useState<string>("");
-    const [summary, setSummary] = useState<React.JSX.Element|null>(null);
+    const [summary, setSummary] = useState<string>("");
+    const [error, setError] = useState<string>("");
     const [results, setResults] = useState<any[]>([]);
     const [loading, setLoading] = useState<boolean>(true);
 
@@ -27,26 +28,21 @@ export default function Search(e: any) {
         if (!query?.length) return;
 
         setLoading(true);
+        setError("");
 
         (async () => {
             const response = await fetch(`/api/search?query=${query}`);
+            const json = await response.json();
 
             setLoading(false);
 
             if (!response.ok) {
-                setSummary(<Error classes="w-fit mx-auto" />);
+                setError(json.error);
                 return;
             }
 
-            const data = await response.json();
-            const formatted = formatSummary(data.summary);
-
-            setSummary(<motion.div className="w-340 rounded-xl px-4 py-3 bg-blue-50 text-blue-400 mx-auto relative max-[700px]:w-full max-[700px]:px-3" initial={{ height: 0, opacity: 0 }} animate={{ height: "auto", opacity: 1 }} transition={{ duration: 1, ease: "easeInOut" }} style={{ overflow: "hidden", transformOrigin: "top center" }}>
-                <h2 className="block mb-2 text-blue-500 font-semibold select-none">Summary</h2>
-                <p className="text-sm leading-relaxed">{formatted}</p>
-            </motion.div>);
-
-            setResults(data.results);
+            setSummary(json.summary);
+            setResults(json.results);
 
             await insertSearchHistory();
         })();
@@ -84,15 +80,27 @@ export default function Search(e: any) {
     return (
         <main className="min-h-[calc(100vh-111px)] grid place-items-center">
             <section className="py-6 w-full max-[700px]:px-3">
-                <div className="w-340 mx-auto mb-3 max-[700px]:w-full">
+                <div className="w-160 mx-auto mb-3 max-[700px]:w-full">
                     <Link href="/" className="group text-sm duration-200 font-medium hover:text-gray-500/75"><FontAwesomeIcon icon={faArrowLeft} className="pr-1 duration-200 group-hover:pr-2" />Back to Search</Link>
                 </div>
-                <div className="w-340 mx-auto py-2 pl-3.5 pr-2 mb-6 rounded-xl border border-gray-300 flex items-center duration-200 justify-between gap-2 has-[input:focus]:border-blue-500 has-[input:focus]:shadow-md max-[700px]:w-full">
+
+                <div className="w-160 mx-auto py-2 pl-3.5 pr-2 mb-6 rounded-xl border border-gray-300 flex items-center duration-200 justify-between gap-2 has-[input:focus]:border-blue-500 has-[input:focus]:shadow-md max-[700px]:w-full">
                     <input type="text" className="w-full focus:outline-hidden text-sm placeholder:text-gray-400/60 placeholder:select-none" placeholder="Start typing..." value={query} readOnly={true} />
                     <Button classes="invisible">Search</Button>
                 </div>
-                {loading ? <div className="w-340 mx-auto select-none text-center font-medium text-gray-400/60 max-[700px]:w-full"><FontAwesomeIcon icon={faCircleNotch} className="animate-spin" /><span className="pl-2">Generating Summary</span></div> : summary}
-                <div className="w-340 mx-auto mb-3 mt-6 max-[700px]:w-full">
+
+                {loading && <div className="w-160 mx-auto select-none text-center font-medium text-gray-400/60 max-[700px]:w-full"><FontAwesomeIcon icon={faCircleNotch} className="animate-spin" /><span className="pl-2">Generating Summary</span></div>}
+
+                {!loading && summary.length > 0 && (
+                    <motion.div className="w-340 rounded-xl px-4 py-3 bg-blue-50 text-blue-400 mx-auto relative max-[700px]:w-full max-[700px]:px-3" initial={{ height: 0, opacity: 0 }} animate={{ height: "auto", opacity: 1 }} transition={{ duration: 1, ease: "easeInOut" }} style={{ overflow: "hidden", transformOrigin: "top center" }}>
+                        <h2 className="block mb-2 text-blue-500 font-semibold select-none">Summary</h2>
+                        <p className="text-sm leading-relaxed">{formatSummary(summary)}</p>
+                    </motion.div>
+                )}
+
+                {!loading && error.length > 0 && <Error classes="w-fit mx-auto" text={error} />}
+
+                <div className="w-160 mx-auto mb-3 mt-6 max-[700px]:w-full">
                     {(loading || results.length) ? null : <h1 className="text-lg font-semibold select-none">Results</h1>}
                     {results.map((result: any, index: number) => (
                         <motion.div key={index} initial={{ opacity: 0, y: 20 }} animate={{ opacity: 1, y: 0 }} transition={{ duration: 0.5, delay: index * 0.1 }}>
